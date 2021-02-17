a6c1858c429b28d1566bee97b71c6ffcc9d0c0a9
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-02-16 21:37:07 -0600

    fix bugs in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mode=$1
 if [ -z "$mode" ]; then mode="vanilla"; fi
 
 echo "build mode: $mode..."
-
+rm -rf fakegopath
 # install kubernetes v1.18.9 if not exist
 if [ ! -d "fakegopath/src/k8s.io/kubernetes" ]
 then
@@ -19,16 +19,18 @@ fi
 if [[ "$mode" == "instr" ]]
 then
   # instrument k8s and install sonar lib
-  ./instrumentation/instr.sh time-travel ${OLDPWD}/fakegopath/src/k8s.io/kubernetes
+  cd instrumentation
+  ./instr.sh staleness ${OLDPWD}/fakegopath/src/k8s.io/kubernetes
+  cd $OLDPWD
 
   rm -rf fakegopath/src/k8s.io/kubernetes/staging/src/sonar.client
   rm -f fakegopath/src/k8s.io/kubernetes/vendor/sonar.client
 
   echo "require sonar.client v0.0.0" >> fakegopath/src/k8s.io/kubernetes/staging/src/k8s.io/apiserver/go.mod
-  echo "replace sonar.client => ../../sonar.client"
+  echo "replace sonar.client => ../../sonar.client" >> fakegopath/src/k8s.io/kubernetes/staging/src/k8s.io/apiserver/go.mod
 
   cp -r sonar.client fakegopath/src/k8s.io/kubernetes/staging/src/sonar.client
-  ln -s fakegopath/src/k8s.io/kubernetes/staging/src/sonar.client fakegopath/src/k8s.io/kubernetes/vendor/sonar.client
+  ln -s ../staging/src/sonar.client fakegopath/src/k8s.io/kubernetes/vendor/sonar.client
 fi
 
 # build kind image
