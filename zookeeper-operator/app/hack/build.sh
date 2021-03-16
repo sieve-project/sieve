85bce2f8455ea2e62f51840701ccb1080cb3d175
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-03-15 19:55:49 -0500

    move everything from zookeeper-operator to test-zookeeper-operator

--- zookeeper-operator/app/hack/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-make build-go -j10
-make build-image
-docker tag zookeeper-operator:latest ramanala/zookeeper-operator:latest
-docker push ramanala/zookeeper-operator:latest
--- zookeeper-operator/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create -f deploy/crds
-kubectl create -f deploy/default_ns/rbac.yaml
-kubectl create -f deploy/default_ns/operator.yaml
-kubectl get deploy
