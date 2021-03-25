18da41441b7c7cdb93ff2cef191e1b3d46ba8042
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-03-25 18:25:28 -0500

    use the same operator.log after restart

--- sonar-server/util.sh
+++ sonar-server/util.sh
@@ -8,12 +8,14 @@ if [ $project = 'cassandra-operator' ]; then
   if [ $mode = 'crash' ]; then
     kubectl exec $pod -- /bin/bash -c "pkill /cassandra-operator"
   elif [ $mode = 'restart' ]; then
-    kubectl exec $pod -- /bin/bash -c "KUBERNETES_SERVICE_HOST=kind-control-plane3 KUBERNETES_SERVICE_PORT=6443 /cassandra-operator &> operator2.log &"
+    kubectl exec $pod -- /bin/bash -c "echo after restart... >> operator.log"
+    kubectl exec $pod -- /bin/bash -c "KUBERNETES_SERVICE_HOST=kind-control-plane3 KUBERNETES_SERVICE_PORT=6443 /cassandra-operator >> operator.log 2>&1 &"
   fi
 elif [ $project = 'zookeeper-operator' ]; then
   if [ $mode = 'crash' ]; then
     kubectl exec $pod -- /bin/bash -c "pkill /usr/local/bin/zookeeper-operator"
   elif [ $mode = 'restart' ]; then
-    kubectl exec $pod -- /bin/bash -c "KUBERNETES_SERVICE_HOST=kind-control-plane3 KUBERNETES_SERVICE_PORT=6443 /usr/local/bin/zookeeper-operator &> operator2.log &"
+    kubectl exec $pod -- /bin/bash -c "echo after restart... >> operator.log"
+    kubectl exec $pod -- /bin/bash -c "KUBERNETES_SERVICE_HOST=kind-control-plane3 KUBERNETES_SERVICE_PORT=6443 /usr/local/bin/zookeeper-operator >> operator.log 2>&1 &"
   fi
 fi
