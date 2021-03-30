654cfa63e9fcf7086c3b92a175e2454c06839b18
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-03-29 21:52:52 -0500

    rabbitmq-operator test1

--- test-rabbitmq-operator/recreateRabbitmqCluster.sh
+++ test-rabbitmq-operator/recreateRabbitmqCluster.sh
@@ -3,20 +3,8 @@
 set -ex
 
 kubectl apply -f config/rmqc-1.yaml
-if [ $1 = 'learn' ]; then sleep 200s; else sleep 50s; fi
-echo ">>>" > stdout.txt
-kubectl get pods >> stdout.txt
-kubectl get sts >> stdout.txt
-kubectl get pvc >> stdout.txt
+sleep 50s
 kubectl delete RabbitmqCluster sonar-rabbitmq-cluster
-if [ $1 = 'learn' ]; then sleep 200s; else sleep 50s; fi
-echo ">>>" >> stdout.txt
-kubectl get pods >> stdout.txt
-kubectl get sts >> stdout.txt
-kubectl get pvc >> stdout.txt
+sleep 50s
 kubectl apply -f config/rmqc-1.yaml
-if [ $1 = 'learn' ]; then sleep 200s; else sleep 50s; fi
-echo ">>>" >> stdout.txt
-kubectl get pods >> stdout.txt
-kubectl get sts >> stdout.txt
-kubectl get pvc >> stdout.txt
+sleep 50s
