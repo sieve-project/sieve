b7f0330eb8d9127dd81e2fed12e4d5a28196505f
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-04-05 16:30:44 -0500

    no need to build for zk

--- test-zookeeper-operator/build/build.sh
+++ test-zookeeper-operator/build/build.sh
@@ -10,7 +10,7 @@ if [ -z "$dockertag" ]; then
     exit 1
 fi
 
-make build-go -j10
+
 make build-image
 docker tag pravega/zookeeper-operator:latest ${dockerrepo}/zookeeper-operator:${dockertag}
 docker push ${dockerrepo}/zookeeper-operator:${dockertag}
