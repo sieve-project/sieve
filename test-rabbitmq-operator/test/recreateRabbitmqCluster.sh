0a6e701bd3bf066caa5f6c759a19d0b0533a89b5
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-04-23 16:16:51 -0500

    rename resource: remove sonar from the names

--- test-rabbitmq-operator/test/recreateRabbitmqCluster.sh
+++ test-rabbitmq-operator/test/recreateRabbitmqCluster.sh
@@ -4,7 +4,7 @@ set -ex
 
 kubectl apply -f rmqc-1.yaml
 sleep 50s
-kubectl delete RabbitmqCluster sonar-rabbitmq-cluster
+kubectl delete RabbitmqCluster rabbitmq-cluster
 sleep 50s
 kubectl apply -f rmqc-1.yaml
 if [ $1 = 'learn' ]; then sleep 100s; else sleep 50s; fi
