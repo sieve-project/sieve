faa8f75454cbd94d9bea9897e9204d8475acd600
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-04-24 16:16:28 -0500

    remove sonar from the resource name of zookeeper operator

--- test-zookeeper-operator/test/scaleDownUpZookeeperCluster.sh
+++ test-zookeeper-operator/test/scaleDownUpZookeeperCluster.sh
@@ -7,4 +7,4 @@ if [ $1 = 'learn' ]; then sleep 400s; else sleep 60s; fi
 kubectl apply -f zkc-1.yaml
 if [ $1 = 'learn' ]; then sleep 300s; else sleep 40s; fi
 kubectl apply -f zkc-2.yaml
-if [ $1 = 'learn' ]; then sleep 150s; else sleep 15s; fi
+if [ $1 = 'learn' ]; then sleep 150s; else sleep 30s; fi
