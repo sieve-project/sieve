5d290249503f431d08d7474946b1408476c38851
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-04-24 19:52:20 -0500

    test-mongodb-operator: remove sonar from the resource name

--- test-mongodb-operator/test/recreateMongodbCluster.sh
+++ test-mongodb-operator/test/recreateMongodbCluster.sh
@@ -4,7 +4,7 @@ set -ex
 
 kubectl apply -f cr.yaml
 sleep 70s
-kubectl delete perconaservermongodb sonar-mongodb-cluster
+kubectl delete perconaservermongodb mongodb-cluster
 sleep 60s
 kubectl apply -f cr.yaml
 sleep 80s
