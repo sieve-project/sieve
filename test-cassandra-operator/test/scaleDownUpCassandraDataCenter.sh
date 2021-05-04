7dbc120aa8e3f2d81ca5aefd1643300fc29b83d1
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-05-03 20:16:52 -0500

    simplify cassandra workload

--- test-cassandra-operator/test/scaleDownUpCassandraDataCenter.sh
+++ test-cassandra-operator/test/scaleDownUpCassandraDataCenter.sh
@@ -4,7 +4,7 @@ set -ex
 
 kubectl apply -f cdc-2.yaml
 if [ $1 = 'learn' ]; then sleep 300s; else sleep 170s; fi
-kubectl apply -f cdc-1.yaml
+kubectl patch CassandraDataCenter cassandra-datacenter --type merge -p='{"spec":{"nodes":1}}'
 if [ $1 = 'learn' ]; then sleep 300s; else sleep 150s; fi
-kubectl apply -f cdc-2.yaml
+kubectl patch CassandraDataCenter cassandra-datacenter --type merge -p='{"spec":{"nodes":2}}'
 if [ $1 = 'learn' ]; then sleep 300s; else sleep 80s; fi
