d704ecfbc1699be67e345e7a5baec39811f75998
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-09-03 16:17:21 -0500

    delete deprecated bash files and sparse-read code

--- test-cassandra-operator/test/scaleDownCassandraDataCenter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-kubectl apply -f cdc-2.yaml
-sleep 200s
-kubectl apply -f cdc-1.yaml
-sleep 150s
--- test-rabbitmq-operator/test/resizePVCRabbitmqCluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-kubectl apply -f rmqc-1.yaml
-sleep 50s
-# kubectl apply -f rmqc-1-15Gi.yaml
-kubectl patch RabbitmqCluster rabbitmq-cluster --type merge -p='{"spec":{"persistence":{"storage":"15Gi"}}}'
-if [ $1 = 'learn' ]; then sleep 100s; else sleep 50s; fi
