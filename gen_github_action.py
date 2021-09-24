6e8e7d4523922cdbde41a1ba01d8f13250b7bbc0
Wenqing Luo <wenqing4@illinois.edu>
2021-09-24 17:54:28 -0500

    fix: ci config error

--- gen_github_action.py
+++ gen_github_action.py
@@ -83,7 +83,7 @@ config_test = {
 }
 
 jobs_daily = copy.deepcopy(jobs)
-jobs_daily['runs-on'] = ['self-hosted', 'linux']
+jobs_daily['runs-on'] = ['self-hosted']
 config_daily = {
     'name': 'Sieve Daily Integration',
     'on': {
