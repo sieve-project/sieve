0c05e890acfcec2e4f1fb78b556e82c3b4e23296
Wenqing Luo <wenqing4@illinois.edu>
2021-09-24 18:23:45 -0500

    fix: run on

--- gen_github_action.py
+++ gen_github_action.py
@@ -6,67 +6,71 @@ from common import sieve_modes
 from datetime import datetime
 import copy
 
-jobs = {}
+def generate_jobs(ci_mode):
+    jobs = {}
 
-for operator in reprod_map:
-    job = {
-        'runs-on': 'ubuntu-latest',
-        'env': {'GOPATH': '/home/runner/go',
-        'KUBECONFIG': '/home/runner/.kube/config'},
-        'steps': [
-            {'uses': 'actions/checkout@v2'},
-            {'name': 'Setup Git',
-             'run': 'git config --global user.name "sieve"\ngit config --global user.email "sieve@sieve.com"'},
-            {'name': 'Setup Go environment',
-             'uses': 'actions/setup-go@v2.1.3',
-             'with': {'go-version': 1.15}},
-            {'name': 'Setup Python',
-             'uses': 'actions/setup-python@v2.2.2',
-             'with': {'python-version': 3.7}},
-            {'name': 'Setup GitHub Package Registry',
-             'run': 'echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u "${{ github.actor }}" --password-stdin'},
-            {'name': 'Install Python Packages',
-             'run': 'pip install -r requirements.txt'},
-            {'name': 'Install Kind', 'run': 'go get sigs.k8s.io/kind\nkind'},
-            {'name': 'Install Mage',
-             'run': 'go get -u github.com/magefile/mage\nmage -h'},
-            {'name': 'Install Helm',
-             'run': 'wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz\ntar -zxvf helm-v3.6.0-linux-amd64.tar.gz\nsudo mv linux-amd64/helm /usr/local/bin/helm\nhelm'},
-            {'name': 'Sieve CI config generate',
-             'run': 'echo "{\\"workload_wait_timeout\\": 600}" > sieve_config.json\ncat sieve_config.json'}
+    for operator in reprod_map:
+        job = {
+            'runs-on': 'ubuntu-latest' if ci_mode == "test" else "self-hosted",
+            'env': {'GOPATH': '/home/runner/go',
+            'KUBECONFIG': '/home/runner/.kube/config'},
+            'steps': [
+                {'uses': 'actions/checkout@v2'},
+                {'name': 'Setup Git',
+                 'run': 'git config --global user.name "sieve"\ngit config --global user.email "sieve@sieve.com"'},
+                {'name': 'Setup Go environment',
+                 'uses': 'actions/setup-go@v2.1.3',
+                 'with': {'go-version': 1.15}},
+                {'name': 'Setup Python',
+                 'uses': 'actions/setup-python@v2.2.2',
+                 'with': {'python-version': 3.7}},
+                {'name': 'Setup GitHub Package Registry',
+                 'run': 'echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u "${{ github.actor }}" --password-stdin'},
+                {'name': 'Install Python Packages',
+                 'run': 'pip install -r requirements.txt'},
+                {'name': 'Install Kind', 'run': 'go get sigs.k8s.io/kind\nkind'},
+                {'name': 'Install Mage',
+                 'run': 'go get -u github.com/magefile/mage\nmage -h'},
+                {'name': 'Install Helm',
+                 'run': 'wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz\ntar -zxvf helm-v3.6.0-linux-amd64.tar.gz\nsudo mv linux-amd64/helm /usr/local/bin/helm\nhelm'},
+                {'name': 'Sieve CI config generate',
+                 'run': 'echo "{\\"workload_wait_timeout\\": 600}" > sieve_config.json\ncat sieve_config.json'}
+            ]
+        }
+        collect_resources = {'uses': 'actions/upload-artifact@v2',
+            'with': {'name': 'sieve-%s-data'%(operator), 'path': 'data/%s'%(operator)}}
+        collect_log = {'uses': 'actions/upload-artifact@v2',
+            'with': {'name': 'sieve-%s-log'%(operator), 'path': 'log'}}
+
+        build_modes = [
+            "learn",
+            sieve_modes.TIME_TRAVEL,
+            sieve_modes.OBS_GAP,
+            sieve_modes.ATOM_VIO
         ]
-    }
-    collect_resources = {'uses': 'actions/upload-artifact@v2',
-        'with': {'name': 'sieve-%s-data'%(operator), 'path': 'data/%s'%(operator)}}
-    collect_log = {'uses': 'actions/upload-artifact@v2',
-        'with': {'name': 'sieve-%s-log'%(operator), 'path': 'log'}}
+        workload_set = set()
 
-    build_modes = [
-        "learn",
-        sieve_modes.TIME_TRAVEL,
-        sieve_modes.OBS_GAP,
-        sieve_modes.ATOM_VIO
-    ]
-    workload_set = set()
-
-    for bug in reprod_map[operator]:
-        workload = reprod_map[operator][bug][0]
-        config_name = reprod_map[operator][bug][1]
-        config = yaml.safe_load(open(os.path.join("reprod", config_name)).read())
-        workload_set.add(workload)
+        for bug in reprod_map[operator]:
+            workload = reprod_map[operator][bug][0]
+            config_name = reprod_map[operator][bug][1]
+            config = yaml.safe_load(open(os.path.join("reprod", config_name)).read())
+            workload_set.add(workload)
 #         print(operator, bug, workload, config['mode'])
 
-    build_image = [{'name': 'Build Image - %s'%(mode), 'run': 'python3 build.py -p %s -m %s -d $IMAGE_NAMESPACE'%(operator, mode)} for mode in build_modes]
-    sieve_learn = [{'name': 'Sieve Learn - %s %s'%(operator, workload), 'run': 'python3 sieve.py -p %s -t %s -s learn -m learn-twice -d $IMAGE_NAMESPACE'%(operator, workload)} for workload in workload_set]
-    sieve_test = [{'name': 'Sieve Test - %s %s'%(operator, bug), 'run': 'python3 reprod.py -p %s -b %s -d $IMAGE_NAMESPACE'%(operator, bug)} for bug in reprod_map[operator].keys()]
-    job['steps'].extend(build_image)
-    job['steps'].extend(sieve_learn)
-    job['steps'].append(collect_resources)
-    job['steps'].extend(sieve_test)
-    job['steps'].append(collect_log)
-    jobs[operator] = job
+        build_image = [{'name': 'Build Image - %s'%(mode), 'run': 'python3 build.py -p %s -m %s -d $IMAGE_NAMESPACE'%(operator, mode)} for mode in build_modes]
+        job['steps'].extend(build_image)
 
-jobs_test = copy.deepcopy(jobs)
+        if not (ci_mode == "test" and operator == "xtradb-operator"):
+            sieve_learn = [{'name': 'Sieve Learn - %s %s'%(operator, workload), 'run': 'python3 sieve.py -p %s -t %s -s learn -m learn-twice -d $IMAGE_NAMESPACE'%(operator, workload)} for workload in workload_set]
+            sieve_test = [{'name': 'Sieve Test - %s %s'%(operator, bug), 'run': 'python3 reprod.py -p %s -b %s -d $IMAGE_NAMESPACE'%(operator, bug)} for bug in reprod_map[operator].keys()]
+            job['steps'].extend(sieve_learn)
+            job['steps'].append(collect_resources)
+            job['steps'].extend(sieve_test)
+            job['steps'].append(collect_log)
+        jobs[operator] = job
+    return jobs
+
+jobs_test = generate_jobs("test")
 config_test = {
     'name': 'Sieve Test',
     'on': {
@@ -76,8 +80,7 @@ config_test = {
     'jobs': jobs_test,
 }
 
-jobs_daily = copy.deepcopy(jobs)
-jobs_daily['runs-on'] = 'self-hosted'
+jobs_daily = generate_jobs("daily")
 config_daily = {
     'name': 'Sieve Daily Integration',
     'on': {
