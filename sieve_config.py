a65e07692aa550d6219f9dabb527b45b72226fbd
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-09-25 20:56:08 -0500

    add flags for different oracles in sieve_config; change default docker repo name to github

--- sieve_config.py
+++ sieve_config.py
@@ -2,13 +2,16 @@ import json
 import os
 
 config = {
-    "docker_repo": "xudongs",
+    "docker_repo": "ghcr.io/sieve-project/action",
     "namespace": "default",
     "time_travel_front_runner": "kind-control-plane",
     "time_travel_straggler": "kind-control-plane3",
     "effect_to_check": ["Delete"],
     "compress_trivial_reconcile": True,
-    "workload_wait_timeout": 600
+    "workload_wait_timeout": 600,
+    "check_digest": True,
+    "check_resource": True,
+    "check_operator_log": True,
 }
 
 if os.path.isfile("sieve_config.json"):
