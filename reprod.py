df88eae5a5d9fe481e5ceab353e7ea11ea219a45
marshtompsxd <xudongsunmarshtomp@gmail.com>
2021-10-23 23:31:50 -0500

    support all for bug option in reprod.py

--- reprod.py
+++ reprod.py
@@ -54,7 +54,7 @@ reprod_map = {
 }
 
 
-def reproduce_bug(operator, bug, docker, phase):
+def reproduce_single_bug(operator, bug, docker, phase):
     mode = bug[:-2]
     test = reprod_map[operator][bug][0]
     config = os.path.join("reprod", reprod_map[operator][bug][1])
@@ -68,6 +68,14 @@ def reproduce_bug(operator, bug, docker, phase):
     )
     cprint(sieve_cmd, bcolors.OKGREEN)
     os.system(sieve_cmd)
+
+
+def reproduce_bug(operator, bug, docker, phase):
+    if bug == "all":
+        for b in reprod_map[operator]:
+            reproduce_single_bug(operator, b, docker, phase)
+    else:
+        reproduce_single_bug(operator, bug, docker, phase)
 
 
 if __name__ == "__main__":
