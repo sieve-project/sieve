70f0ed56efcf2b4e8ef23cbed5219d58666dfba9
tylergu <jiaweig3@illinois.edu>
2021-12-02 15:30:11 -0600

    add xtradb obs gap 1 reprod

--- reprod.py
+++ reprod.py
@@ -39,6 +39,7 @@ reprod_map = {
     "xtradb-operator": {
         "atom-vio-1": ["disable-enable-proxysql", "xtradb_atom_vio_1.yaml"],
         "atom-vio-2": ["run-cert-manager", "xtradb_atom_vio_2.yaml"],
+        "obs-gap-1": ["scaleup-scaledown", "xtradb_obs_gap_1.yaml"],
         "time-travel-1": ["recreate", "xtradb_time_travel_1.yaml"],
         "time-travel-2": ["disable-enable-haproxy", "xtradb_time_travel_2.yaml"],
         "time-travel-3": ["disable-enable-proxysql", "xtradb_time_travel_3.yaml"],
