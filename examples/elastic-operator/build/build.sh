bd286255370aba9948076f3b61b0e3840df79774
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-01-08 22:17:47 -0600

    check in build script

---
+++ examples/elastic-operator/build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+dockerrepo=$1
+dockertag=$2
+if [ -z "$dockerrepo" ]; then
+    exit 1
+fi
+if [ -z "$dockertag" ]; then
+    exit 1
+fi
+
+sed -i 's/go-generate generate-config-file/go-generate /g'  Makefile
+
+OPERATOR_IMAGE=${dockerrepo}/elastic-operator:${dockertag} make docker-build
+docker push ${dockerrepo}/elastic-operator:${dockertag}
