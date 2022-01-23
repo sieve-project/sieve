fe8ca27622e1653b575d320c5aede52be24f8003
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-01-22 19:43:25 -0600

    scripts for checking plan generation

---
+++ check_test_plan_gen.py
@@ -0,0 +1,13 @@
+from evaluation_sanity_check import check, generate
+import sys
+import os
+
+current_dir = "log"
+previous_dir = sys.argv[1]
+
+os.system("rm -rf %s" % current_dir)
+os.system("cp -r %s %s" % (previous_dir, current_dir))
+
+generate.generate_test_plan_stat()
+check.check_massive_testing_results(current_dir, previous_dir)
+check.check_bug_reproduction_test_plans()
---
+++ evaluation_sanity_check/common.py
@@ -0,0 +1,11 @@
+controllers_to_check = [
+    "cassandra-operator",
+    "zookeeper-operator",
+    "rabbitmq-operator",
+    "mongodb-operator",
+    "cass-operator",
+    "casskop-operator",
+    "xtradb-operator",
+    "yugabyte-operator",
+    "nifikop-operator",
+]
