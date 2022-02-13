6ffed47941536f32bf91ad3aba5c4862ac7ef49e
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-02-12 20:31:45 -0600

    disable check.check_massive_testing_results in check_test_plan_gen.py

--- check_test_plan_gen.py
+++ check_test_plan_gen.py
@@ -2,12 +2,12 @@ from evaluation_sanity_check import check, generate
 import sys
 import os
 
-current_dir = "log"
-previous_dir = sys.argv[1]
+# current_dir = "log"
+# previous_dir = sys.argv[1]
 
-os.system("rm -rf %s" % current_dir)
-os.system("cp -r %s %s" % (previous_dir, current_dir))
+# os.system("rm -rf %s" % current_dir)
+# os.system("cp -r %s %s" % (previous_dir, current_dir))
 
 generate.generate_test_plan_stat()
-check.check_massive_testing_results(current_dir, previous_dir)
+# check.check_massive_testing_results(current_dir, previous_dir)
 check.check_bug_reproduction_test_plans()
