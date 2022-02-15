79cc962f908ef7509288b2d79f6b85b62ed120fa
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-02-15 00:30:23 -0600

    update contour config and test

--- examples/contour/test/test.py
+++ examples/contour/test/test.py
@@ -6,11 +6,10 @@ sieve_root = os.path.dirname(os.path.dirname(os.path.dirname(current)))
 sys.path.append(sieve_root)
 
 from sieve_test_driver.test_framework import new_built_in_workload
-from sieve_common.common import RUNNING, TERMINATED
 
 test_cases = {
-    "create": new_built_in_workload(100)
-    .cmd("kubectl apply -f examples/contour-operator/test/gateway.yaml")
+    "create": new_built_in_workload(50)
+    .cmd("kubectl apply -f examples/contour/test/gateway.yaml")
     .cmd("kubectl apply -f examples/contour/test/kuard.yaml")
 }
 
