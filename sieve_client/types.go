32f8d72a84e9693a51604adf634435a7d265e609
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-02-23 17:58:15 -0600

    remove request types for deprecated clients

--- sieve_client/types.go
+++ sieve_client/types.go
@@ -10,84 +10,6 @@ type EchoRequest struct {
 	Text string
 }
 
-type NotifyStaleStateBeforeProcessEventRequest struct {
-	EventType    string
-	ResourceType string
-	Hostname     string
-}
-
-type NotifyStaleStateCrucialEventRequest struct {
-	Hostname  string
-	EventType string
-	Object    string
-}
-
-type NotifyStaleStateRestartPointRequest struct {
-	Hostname     string
-	EventType    string
-	ResourceType string
-	Name         string
-	Namespace    string
-}
-
-type NotifyStaleStateAfterSideEffectsRequest struct {
-	SideEffectID   int
-	SideEffectType string
-	Object         string
-	ResourceType   string
-	Error          string
-}
-
-type NotifyUnobsrStateAfterSideEffectsRequest struct {
-	SideEffectID   int
-	SideEffectType string
-	Object         string
-	ResourceType   string
-	Error          string
-}
-
-type NotifyUnobsrStateBeforeIndexerWriteRequest struct {
-	OperationType string
-	Object        string
-	ResourceType  string
-}
-
-type NotifyUnobsrStateBeforeInformerCacheReadRequest struct {
-	OperationType string
-	ResourceType  string
-	Name          string
-	Namespace     string
-}
-
-type NotifyUnobsrStateAfterInformerCacheReadRequest struct {
-	OperationType string
-	ResourceType  string
-	Name          string
-	Namespace     string
-}
-
-type NotifyUnobsrStateAfterIndexerWriteRequest struct {
-	OperationType string
-	Object        string
-	ResourceType  string
-}
-
-type NotifyIntmdStateAfterSideEffectsRequest struct {
-	SideEffectID   int
-	SideEffectType string
-	Object         string
-	ResourceType   string
-	ReconcilerType string
-	Error          string
-}
-
-type NotifyIntmdStateAfterNonK8sSideEffectsRequest struct {
-	SideEffectID   int
-	RecvTypeName   string
-	FunName        string
-	ReconcilerType string
-}
-
 type NotifyLearnBeforeIndexerWriteRequest struct {
 	OperationType string
 	Object        string
@@ -143,22 +65,6 @@ type NotifyLearnAfterOperatorGetRequest struct {
 
 type NotifyLearnAfterOperatorListRequest struct {
 	FromCache      bool
-	ResourceType   string
-	ObjectList     string
-	ReconcilerType string
-	Error          string
-}
-
-type NotifyIntmdStateAfterOperatorGetRequest struct {
-	ResourceType   string
-	Namespace      string
-	Name           string
-	Object         string
-	ReconcilerType string
-	Error          string
-}
-
-type NotifyIntmdStateAfterOperatorListRequest struct {
 	ResourceType   string
 	ObjectList     string
 	ReconcilerType string
