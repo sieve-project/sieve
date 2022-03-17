b4188dab784434b806731d38a2fabc2656eab6f2
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-03-17 16:29:11 -0500

    Split daily integration into multiple actions

--- gen_github_action.py
+++ gen_github_action.py
@@ -6,9 +6,9 @@ from sieve_common.common import sieve_modes
 from datetime import datetime
 
 operators_for_CI = {
+    "cass-operator": ["recreate", "scaledown-scaleup"],
     "cassandra-operator": ["recreate", "scaledown-scaleup"],
-    "zookeeper-operator": ["recreate", "scaledown-scaleup"],
-    "rabbitmq-operator": ["recreate", "scaleup-scaledown", "resize-pvc"],
+    "casskop-operator": ["recreate", "scaledown-to-zero", "reducepdb"],
     "mongodb-operator": [
         "recreate",
         "scaleup-scaledown",
@@ -16,8 +16,8 @@ operators_for_CI = {
         "disable-enable-arbiter",
         "run-cert-manager",
     ],
-    "cass-operator": ["recreate", "scaledown-scaleup"],
-    "casskop-operator": ["recreate", "scaledown-to-zero", "reducepdb"],
+    "nifikop-operator": ["recreate", "scaledown-scaleup", "change-config"],
+    "rabbitmq-operator": ["recreate", "scaleup-scaledown", "resize-pvc"],
     "xtradb-operator": [
         "recreate",
         "disable-enable-haproxy",
@@ -31,118 +31,183 @@ operators_for_CI = {
         "disable-enable-tls",
         "disable-enable-tuiport",
     ],
-    "nifikop-operator": ["recreate", "scaledown-scaleup", "change-config"],
+    "zookeeper-operator": ["recreate", "scaledown-scaleup"],
 }
 
 
-def generate_jobs(ci_mode):
+def job_template(self_hosted):
+    return {
+        "runs-on": "self-hosted" if self_hosted else "ubuntu-latest",
+        "env": {
+            "GOPATH": "/home/runner/go",
+            "KUBECONFIG": "/home/runner/.kube/config",
+        },
+        "steps": [
+            {"uses": "actions/checkout@v2"},
+            {
+                "name": "Setup Git",
+                "run": 'git config --global user.name "sieve"\ngit config --global user.email "sieve@sieve.com"',
+            },
+            {
+                "name": "Setup Go environment",
+                "uses": "actions/setup-go@v2.1.3",
+                "with": {"go-version": 1.15},
+            },
+            {
+                "name": "Setup Python",
+                "uses": "actions/setup-python@v2.2.2",
+                "with": {"python-version": 3.7},
+            },
+            {
+                "name": "Setup GitHub Package Registry",
+                "run": 'echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u "${{ github.actor }}" --password-stdin',
+            },
+            {
+                "name": "Install Python Packages",
+                "run": "pip install -r requirements.txt",
+            },
+            {"name": "Install Kind", "run": "go get sigs.k8s.io/kind\nkind"},
+            {
+                "name": "Install Mage",
+                "run": "go get -u github.com/magefile/mage\nmage -h",
+            },
+            {
+                "name": "Install Helm",
+                "run": "wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz\ntar -zxvf helm-v3.6.0-linux-amd64.tar.gz\nsudo mv linux-amd64/helm /usr/local/bin/helm\nhelm",
+            },
+            {
+                "name": "Sieve CI config generate",
+                "run": 'echo "{\\"workload_hard_timeout\\": 1000}" > sieve_config.json\ncat sieve_config.json',
+            },
+        ],
+    }
+
+
+def collect_log_step(operator):
+    return {
+        "uses": "actions/upload-artifact@v2",
+        "if": "always()",
+        "with": {"name": "sieve-%s-log" % (operator), "path": "log"},
+    }
+
+
+def persistent_data_step(operator):
+    return {
+        "uses": "JamesIves/github-pages-deploy-action@4.1.5",
+        "name": "Persistent oracle data",
+        "with": {
+            "branch": "oracle-data",
+            "folder": "examples/%s/oracle" % (operator),
+            "target-folder": "%s/oracle" % (operator),
+        },
+    }
+
+
+def remove_cluster_step():
+    return {
+        "name": "Remove cluster",
+        "if": "always()",
+        "run": "kind delete cluster",
+    }
+
+
+def clean_images_step():
+    return {
+        "name": "Clean images",
+        "if": "always()",
+        "run": "docker image prune -a -f && docker builder prune -a -f && docker system df",
+    }
+
+
+def generate_controller_image_build_jobs(self_hosted):
     jobs = {}
+    for operator in operators_for_CI:
+        job = job_template(self_hosted)
+        build_modes = [
+            "learn",
+            "test",
+            "vanilla",
+        ]
+        build_image = [
+            {
+                "name": "Build Image - %s" % (mode),
+                "run": "python3 build.py -p %s -m %s -d $IMAGE_NAMESPACE -r"
+                % (operator, mode),
+            }
+            for mode in build_modes
+        ]
+        job["steps"].extend(build_image)
+        job["steps"].append(clean_images_step())
+        jobs[operator] = job
+    return jobs
 
-    for operator in reprod_map:
-        if operator not in operators_for_CI:
-            continue
-        job = {
-            "runs-on": "ubuntu-latest" if ci_mode == "test" else "self-hosted",
-            "env": {
-                "GOPATH": "/home/runner/go",
-                "KUBECONFIG": "/home/runner/.kube/config",
-            },
-            "steps": [
-                {"uses": "actions/checkout@v2"},
-                {
-                    "name": "Setup Git",
-                    "run": 'git config --global user.name "sieve"\ngit config --global user.email "sieve@sieve.com"',
-                },
-                {
-                    "name": "Setup Go environment",
-                    "uses": "actions/setup-go@v2.1.3",
-                    "with": {"go-version": 1.15},
-                },
-                {
-                    "name": "Setup Python",
-                    "uses": "actions/setup-python@v2.2.2",
-                    "with": {"python-version": 3.7},
-                },
-                {
-                    "name": "Setup GitHub Package Registry",
-                    "run": 'echo "${{ secrets.GITHUB_TOKEN }}" | docker login ghcr.io -u "${{ github.actor }}" --password-stdin',
-                },
-                {
-                    "name": "Install Python Packages",
-                    "run": "pip install -r requirements.txt",
-                },
-                {"name": "Install Kind", "run": "go get sigs.k8s.io/kind\nkind"},
-                {
-                    "name": "Install Mage",
-                    "run": "go get -u github.com/magefile/mage\nmage -h",
-                },
-                {
-                    "name": "Install Helm",
-                    "run": "wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz\ntar -zxvf helm-v3.6.0-linux-amd64.tar.gz\nsudo mv linux-amd64/helm /usr/local/bin/helm\nhelm",
-                },
-                {
-                    "name": "Sieve CI config generate",
-                    "run": 'echo "{\\"workload_hard_timeout\\": 1000}" > sieve_config.json\ncat sieve_config.json',
-                },
-            ],
-        }
-        collect_log = {
-            "uses": "actions/upload-artifact@v2",
-            "if": "always()",
-            "with": {"name": "sieve-%s-log" % (operator), "path": "log"},
-        }
-        persistent_data = {
-            "uses": "JamesIves/github-pages-deploy-action@4.1.5",
-            "name": "Persistent oracle data",
-            "with": {
-                "branch": "oracle-data",
-                "folder": "examples/%s/oracle" % (operator),
-                "target-folder": "%s/oracle" % (operator),
-            },
-        }
-        remove_cluster = {
-            "name": "Remove cluster",
-            "if": "always()",
-            "run": "kind delete cluster",
-        }
-        clean_images = {
-            "name": "Clean images",
-            "if": "always()",
-            "run": "docker image prune -a -f && docker builder prune -a -f && docker system df",
-        }
 
+def generate_oracle_build_jobs(self_hosted):
+    jobs = {}
+    for operator in operators_for_CI:
+        job = job_template(self_hosted)
+        workload_set = set(operators_for_CI[operator])
+        sieve_learn = [
+            {
+                "name": "Sieve Learn - %s %s" % (operator, workload),
+                "run": "python3 sieve.py -p %s -t %s -s learn -m learn-twice -d $IMAGE_NAMESPACE"
+                % (operator, workload),
+            }
+            for workload in sorted(workload_set)
+        ]
+        job["steps"].extend(sieve_learn)
+        job["steps"].append(collect_log_step(operator))
+        job["steps"].append(remove_cluster_step())
+        job["steps"].append(persistent_data_step(operator))
+        job["steps"].append(clean_images_step())
+        jobs[operator] = job
+    return jobs
+
+
+def generate_bug_reproduction_jobs(self_hosted):
+    jobs = {}
+    for operator in operators_for_CI:
+        job = job_template(self_hosted)
+        sieve_test = []
+        for bug in sorted(reprod_map[operator].keys()):
+            if "indirect" in bug:
+                continue
+            sieve_test.append(
+                {
+                    "name": "Sieve Test - %s %s" % (operator, bug),
+                    "run": "python3 reproduce_bugs.py -p %s -b %s -d $IMAGE_NAMESPACE"
+                    % (operator, bug),
+                }
+            )
+        job["steps"].extend(sieve_test)
+        job["steps"].append(collect_log_step(operator))
+        job["steps"].append(remove_cluster_step())
+        job["steps"].append(persistent_data_step(operator))
+        job["steps"].append(clean_images_step())
+        jobs[operator] = job
+    return jobs
+
+
+def generate_test_jobs(self_hosted):
+    jobs = {}
+    for operator in operators_for_CI:
+        job = job_template(self_hosted)
         build_modes = [
             "learn",
             "test",
             sieve_modes.VANILLA,
         ]
-        workload_set = set()
-
-        for bug in reprod_map[operator]:
-            if "indirect" in bug:
-                continue
-            workload = reprod_map[operator][bug][0]
-            config_name = reprod_map[operator][bug][1]
-            config = yaml.safe_load(
-                open(os.path.join("bug_reproduction_test_plans", config_name)).read()
-            )
-            workload_set.add(workload)
-
-        for workload in operators_for_CI[operator]:
-            workload_set.add(workload)
-
+        workload_set = set(operators_for_CI[operator])
         build_image = [
             {
                 "name": "Build Image - %s" % (mode),
                 "run": "python3 build.py -p %s -m %s -d $IMAGE_NAMESPACE"
-                % (operator, mode)
-                + (" -r" if ci_mode == "daily" else ""),
+                % (operator, mode),
             }
             for mode in build_modes
         ]
         job["steps"].extend(build_image)
-
-        if not (ci_mode in ["test"] and operator == "xtradb-operator"):
+        if not (operator == "xtradb-operator"):
             sieve_learn = [
                 {
                     "name": "Sieve Learn - %s %s" % (operator, workload),
@@ -164,43 +229,72 @@ def generate_jobs(ci_mode):
                 )
             job["steps"].extend(sieve_learn)
             job["steps"].extend(sieve_test)
-            job["steps"].append(collect_log)
-            job["steps"].append(remove_cluster)
-            if ci_mode == "daily":
-                job["steps"].append(persistent_data)
-                job["steps"].append(clean_images)
+            job["steps"].append(collect_log_step(operator))
+            job["steps"].append(remove_cluster_step())
         jobs[operator] = job
     return jobs
 
 
-jobs_test = generate_jobs("test")
-config_test = {
-    "name": "Sieve Test",
-    "on": {"pull_request": None, "workflow_dispatch": None},
-    "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
-    "jobs": jobs_test,
-}
-
-jobs_daily = generate_jobs("daily")
-config_daily = {
-    "name": "Sieve Daily Integration",
-    "on": {"workflow_dispatch": None, "schedule": [{"cron": "0 6 * * *"}]},
-    "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
-    "jobs": jobs_daily,
-}
-
-# dump
-config_path_test = ".github/workflows/sieve-test.yml"
-config_path_daily = ".github/workflows/sieve-daily.yml"
-open(config_path_test, "w").write(
+open(".github/workflows/sieve-test.yml", "w").write(
     "# This file is automatically generated by gen_github_action.py on %s\n"
     % (datetime.now())
-    + yaml.dump(config_test, default_flow_style=False, sort_keys=False)
+    + yaml.dump(
+        {
+            "name": "Sieve Test",
+            "on": {"pull_request": None, "workflow_dispatch": None},
+            "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
+            "jobs": generate_test_jobs(False),
+        },
+        default_flow_style=False,
+        sort_keys=False,
+        width=1000,
+    )
 )
-print("CI config generated to %s" % (config_path_test))
-open(config_path_daily, "w").write(
+
+open(".github/workflows/sieve-controller-image-build.yml", "w").write(
     "# This file is automatically generated by gen_github_action.py on %s\n"
     % (datetime.now())
-    + yaml.dump(config_daily, default_flow_style=False, sort_keys=False)
+    + yaml.dump(
+        {
+            "name": "Sieve Controller Image Build",
+            "on": {"workflow_dispatch": None, "schedule": [{"cron": "0 6 * * *"}]},
+            "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
+            "jobs": generate_controller_image_build_jobs(False),
+        },
+        default_flow_style=False,
+        sort_keys=False,
+        width=1000,
+    )
 )
-print("CI config generated to %s" % (config_path_daily))
+
+open(".github/workflows/sieve-oracle-build.yml", "w").write(
+    "# This file is automatically generated by gen_github_action.py on %s\n"
+    % (datetime.now())
+    + yaml.dump(
+        {
+            "name": "Sieve Oracle Build",
+            "on": {"workflow_dispatch": None},
+            "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
+            "jobs": generate_oracle_build_jobs(True),
+        },
+        default_flow_style=False,
+        sort_keys=False,
+        width=1000,
+    )
+)
+
+open(".github/workflows/sieve-bug-reproduction.yml", "w").write(
+    "# This file is automatically generated by gen_github_action.py on %s\n"
+    % (datetime.now())
+    + yaml.dump(
+        {
+            "name": "Sieve Bug Reproduction",
+            "on": {"workflow_dispatch": None},
+            "env": {"IMAGE_NAMESPACE": "ghcr.io/sieve-project/action"},
+            "jobs": generate_bug_reproduction_jobs(True),
+        },
+        default_flow_style=False,
+        sort_keys=False,
+        width=1000,
+    )
+)
