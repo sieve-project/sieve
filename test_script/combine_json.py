7ef73c3a7aa1b23b7a2f32c1d5786efb8be8a941
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-03-17 21:28:24 -0500

    fix bug in combine_json.py about generating json file name

--- test_script/combine_json.py
+++ test_script/combine_json.py
@@ -3,39 +3,53 @@ import json
 import time
 import os
 
+
 def merge(result, patch):
-    '''
+    """
     Recursive function to merge two dict
-    '''
+    """
     for key in patch:
         if key not in result:
             result[key] = patch[key]
         else:
             if type(patch[key]) is not dict:
-                print('ERROR: Duplicate config, overwriting')
+                print("ERROR: Duplicate config, overwriting")
                 result[key] = patch[key]
             else:
                 merge(result[key], patch[key])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     t = time.localtime()
-    json_names = glob.glob('../sieve_test_results/*.json')
+    json_names = glob.glob("../sieve_test_results/*.json")
 
     result = {}
-    result['failed'] = []
+    result["failed"] = []
     for fname in json_names:
-        with open(fname, 'r') as in_json:
+        with open(fname, "r") as in_json:
             patch = json.load(in_json)
             merge(result, patch)
 
-    with open('commands.txt', 'r') as command_file:
+    with open("commands.txt", "r") as command_file:
         commands = command_file.read().splitlines()
         for command in commands:
             args = command.split()
-            result_name = '%s-%s-%s' % (args[5], args[9], os.path.splitext(os.path.basename(args[11]))[0])
+            operator_name = args[7]
+            workload_name = args[9]
+            test_plan_name_without_ext = os.path.splitext(os.path.basename(args[11]))[0]
+            result_name = "%s-%s-%s" % (
+                operator_name,
+                workload_name,
+                test_plan_name_without_ext,
+            )
             if not any(result_name in s for s in json_names):
                 print(result_name)
-                result['failed'].append(command)
+                result["failed"].append(command)
 
-    with open('test-summary-{}-{}-{}-{}-{}.json'.format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min), 'w') as merged:
+    with open(
+        "test-summary-{}-{}-{}-{}-{}.json".format(
+            t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min
+        ),
+        "w",
+    ) as merged:
         json.dump(result, merged, indent=4, sort_keys=True)
