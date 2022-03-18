9b422de848011fd6cb99435084cb8d56b6a94497
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-03-17 23:46:24 -0500

    generate tsv from the test result json files

--- test_script/json_to_tsv.py
+++ test_script/json_to_tsv.py
@@ -3,6 +3,9 @@ import sys
 import os
 
 result_map = json.load(open(sys.argv[1]))
+print(
+    "controller\tworkload\tpattern\ttest plan\tduration\thost\tworkload complated\tinjection completed\tno exception\tnumber errors"
+)
 for operator in result_map:
     for test_case in result_map[operator]:
         for mode in result_map[operator][test_case]:
@@ -10,15 +13,41 @@ for operator in result_map:
                 short_config = os.path.basename(config)
                 duration = result_map[operator][test_case][mode][config]["duration"]
                 host = result_map[operator][test_case][mode][config]["host"]
-                messages = result_map[operator][test_case][mode][config]["messages"]
-                ret_val = result_map[operator][test_case][mode][config]["ret_val"]
+                workload_completed = result_map[operator][test_case][mode][config][
+                    "workload_completed"
+                ]
+                injection_completed = result_map[operator][test_case][mode][config][
+                    "injection_completed"
+                ]
+                no_exception = result_map[operator][test_case][mode][config][
+                    "no_exception"
+                ]
+                number_errors = result_map[operator][test_case][mode][config][
+                    "number_errors"
+                ]
+                # detected_errors = "\n".join(
+                #     result_map[operator][test_case][mode][config]["detected_errors"]
+                # )
+                # test_config_content = result_map[operator][test_case][mode][config][
+                #     "test_config_content"
+                # ]
+                # exception_message = result_map[operator][test_case][mode][config][
+                #     "exception_message"
+                # ]
                 print(
-                    "{}\t{}\t{}\t{}\t{}\t{}".format(
+                    "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
                         operator,
                         test_case,
                         mode,
                         short_config,
-                        ret_val,
                         duration,
+                        host,
+                        workload_completed,
+                        injection_completed,
+                        no_exception,
+                        number_errors,
+                        # detected_errors,
+                        # test_config_content,
+                        # exception_message,
                     )
                 )
