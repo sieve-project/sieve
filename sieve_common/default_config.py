0d274da7ee6e5b81aec086e4dd401260ee5e8909
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-03-27 17:40:05 -0500

    add end_state_checker_mask into config

--- sieve_common/default_config.py
+++ sieve_common/default_config.py
@@ -26,6 +26,7 @@ class CommonConfig:
         field_key_mask,
         field_path_mask,
         state_update_summary_checker_mask,
+        end_state_checker_mask,
     ):
         self.docker_registry = docker_registry
         self.controller_folder = controller_folder
@@ -52,6 +53,7 @@ class CommonConfig:
         self.field_key_mask = field_key_mask
         self.field_path_mask = field_path_mask
         self.state_update_summary_checker_mask = state_update_summary_checker_mask
+        self.end_state_checker_mask = end_state_checker_mask
 
 
 def get_common_config():
@@ -99,6 +101,7 @@ def get_common_config():
         state_update_summary_checker_mask=common_config[
             "state_update_summary_checker_mask"
         ],
+        end_state_checker_mask=common_config["end_state_checker_mask"],
     )
 
 
