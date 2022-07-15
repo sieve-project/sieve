b202db05a195230a2c0c91b944f8386437b794fe
marshtompsxd <xudongsunmarshtomp@gmail.com>
2022-07-15 05:28:32 +0000

    implement example foo safety checker

--- sieve_oracle/customized_safety_checker.py
+++ sieve_oracle/customized_safety_checker.py
@@ -10,17 +10,33 @@ class SafetyCheckerSuite:
 
 # Customized safety checker example:
 # This checker checks a safety property:
-# The rattbitmq-cluster-server statefulset should never have more than one replica
+# The rabbitmq-cluster-server statefulset should never have more than one replica
 # (the property is not correct; this is just an example)
 # Sieve will apply this checker to every state
 # As long as there is one state where the property does not hold (return false in the function)
 # Sieve will report an alarm
-def example_safety_checker(state):
+def example_rabbitmq_safety_checker(state):
     key = "statefulset/default/rabbitmq-cluster-server"
     if key in state:
         object_state = json.loads(state[key])
-        # print(object_state)
         if object_state["Spec"]["Replicas"] > 1:
+            return False
+    return True
+
+
+# This is another example that is similar to the checker mentioned in
+# https://github.com/sieve-project/sieve/issues/42
+def example_foo_safety_checker(state):
+    key1 = "statefulset/default/sts1"
+    key2 = "statefulset/default/sts2"
+    ratio = 0.7
+    if key1 in state and key2 in state:
+        object_state1 = json.loads(state[key1])
+        object_state2 = json.loads(state[key2])
+        if (
+            object_state1["Spec"]["Replicas"] * ratio
+            < object_state2["Spec"]["Replicas"]
+        ):
             return False
     return True
 
@@ -34,6 +50,6 @@ customized_safety_checker_suites = [
     # SafetyCheckerSuite(
     #     ["statefulset/default/rabbitmq-cluster-server"],
     #     "example_checker",
-    #     example_safety_checker,
+    #     example_rabbitmq_safety_checker,
     # )
 ]
